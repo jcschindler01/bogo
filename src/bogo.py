7e4af7cfe21c60469a2557166339bb67bf6c7300
EricSanchezLL <ericthebts@gmail.com>
2023-06-01 09:46:07 +0200

    First modifications on bogo code

--- src/bogo.py
+++ src/bogo.py
@@ -22,6 +22,16 @@ import numpy as np
 ##### helper functions ######
 #############################
 
+# eigendecomposition of M (we don't want to include this process in each function!)
+def R(M):
+    vals, vecs = np.linalg.eig(M)
+    sorted_indices = np.argsort(vals)
+    sorted_eigenvalues = vals[sorted_indices]
+    sorted_eigenvectors = vecs[:, sorted_indices]
+
+    return sorted_eigenvalues, sorted_eigenvectors.T
+    
+
 
 
 
@@ -29,18 +39,18 @@ import numpy as np
 ###########################################
 
 ## local to intermediate
-def ab(na,nb,M):
+def ab(na,nb,R):
     """Calculate <na|nb> from local (a) to intermediate (b=Ra) modes."""
     return 0
 
 ## intermediate to global
-def bA(nb,nA,M):
-    """Calculate <na|nb> from local (a) to intermediate (b=Ra) modes."""
+def bA(nb,nA,R):
+    """Calculate <nb|nA> from intermediate (b) to global (A) modes."""
     return 0
 
 ## local to global
-def ab(na,nA,M):
-    """Calculate <na|nb> from local (a) to intermediate (b=Ra) modes."""
+def ab(na,nA,R):
+    """Calculate <na|nb> from local (a) to global (A) modes."""
     return 0
 
 
